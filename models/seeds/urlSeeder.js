ea77ee4df5b6c63491e88d601483883e2a25a2a2
AC-Galen <lottaie7359882@gmail.com>
2022-05-30 16:18:37 +0800

    feat: add url seeder

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,30 +1,18 @@
-const express = require('express')
-const exhbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const app = express()
+const url = require('../url')
 
 mongoose.connect(process.env.SHORT_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('mongodb error')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-})
 
-
-
-
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('This is first page')
-})
-
-
-
-
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
+  for (let i = 0; i < 5; i++) {
+    url.create({ url: `url-${i}`})
+  }
+  console.log('done')
 })
